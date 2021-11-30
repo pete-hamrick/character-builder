e1cda97bec78d8b631fd479afa18f498dfef6b90
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-29 19:10:06 -0800

    added character display logic, having eslint/prettier issue so I cannot npm start

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -3,7 +3,45 @@ import React from 'react'
 export default function Character() {
   return (
     <section>
-      <h1>Character Display</h1>
+      <article
+        className="hat"
+        style={{
+          backgroundImage: `url(./${hat}-hat.png)`,
+          height: 150,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></article>
+
+      <article
+        className="head"
+        style={{
+          backgroundImage: `url(./${head}-head.png)`,
+          height: 150,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></article>
+
+      <article
+        className="shirt"
+        style={{
+          backgroundImage: `url(./${shirt}-shirt.png)`,
+          height: 150,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></article>
+
+      <article
+        className="bottom"
+        style={{
+          backgroundImage: `url(./${bottom}-bottom.png)`,
+          height: 150,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></article>
     </section>
   )
 }
