b58edd54648ade7fa6e486d4d6d1720e743d3a6e
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-29 18:55:45 -0800

    added drop down options in editor component

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -13,26 +13,47 @@ export default function Editor({
   setNewCatchphrase,
   handleClick,
 }) {
+  const hatPics = ['beanie', 'derby', 'fishing', 'trucker']
+  const headPics = ['bear', 'fish', 'otter', 'sloth']
+  const shirtPics = ['fishing', 'tuxedo']
+  const bottomPics = ['kilt', 'yeti']
+
   return (
     <section>
       <label>
         Hat:
-        <select value={hat} onChange={(e) => onHatChange(e.target.value)}></select>
+        <select value={hat} onChange={(e) => onHatChange(e.target.value)}>
+          {hatPics.map((pic) => (
+            <option key={pic}>{pic}</option>
+          ))}
+        </select>
       </label>
 
       <label>
         Head:
-        <select value={head} onChange={(e) => onHeadChange(e.target.value)}></select>
+        <select value={head} onChange={(e) => onHeadChange(e.target.value)}>
+          {headPics.map((pic) => (
+            <option key={pic}>{pic}</option>
+          ))}
+        </select>
       </label>
 
       <label>
         Shirt:
-        <select value={shirt} onChange={(e) => onShirtChange(e.target.value)}></select>
+        <select value={shirt} onChange={(e) => onShirtChange(e.target.value)}>
+          {shirtPics.map((pic) => (
+            <option key={pic}>{pic}</option>
+          ))}
+        </select>
       </label>
 
       <label>
         Bottom:
-        <select value={bottom} onChange={(e) => onBottomChange(e.target.value)}></select>
+        <select value={bottom} onChange={(e) => onBottomChange(e.target.value)}>
+          {bottomPics.map((pic) => (
+            <option key={pic}>{pic}</option>
+          ))}
+        </select>
       </label>
 
       <label>
